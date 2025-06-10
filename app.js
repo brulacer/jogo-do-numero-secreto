3ec53fffde6832df6d26d60b7bf4c58ba0ba7929
brulacer <brulacer@gmail.com>
2025-06-10 19:48:05 -0300

    alterar limite do número sorteado para 10000

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 2000;
+let numeroLimite = 10000;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensgemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 2.000');    
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10.000');    
 }
 
 exibirMensgemInicial();
